5c3c19765ddde728c9fac1f7e9c023c6427498f6
DESKTOP-POL0H0E\Salim <salimbenkhaled93@gmail.com>
2023-01-14 13:23:50 -0700

    added test for GetPokemonDetailsViewModel

--- app/src/test/java/com/salim/mypokedex/pokemonDetails/GetPokemonDetailsUseCaseTest.kt
+++ app/src/test/java/com/salim/mypokedex/pokemonDetails/GetPokemonDetailsUseCaseTest.kt
@@ -1,5 +1,7 @@
 package com.salim.mypokedex.pokemonDetails
 
+import com.salim.mypokedex.PokemonTestData.TEST_POKEMON
+import com.salim.mypokedex.PokemonTestData.TEST_POKEMON_DETAIL_SCHEMA
 import com.salim.mypokedex.networking.PokedexApiService
 import com.salim.mypokedex.pokemon.*
 import io.mockk.*
@@ -8,7 +10,6 @@ import io.mockk.impl.annotations.RelaxedMockK
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.last
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 
@@ -152,177 +153,5 @@ class GetPokemonDetailsUseCaseTest {
     companion object {
         private const val POKEMON_NAME = "bulbasaur"
 
-        private val TEST_POKEMON = Pokemon(
-            baseExperience = 0,
-            height = 0,
-            id = 0,
-            isDefault = false,
-            locationAreaEncounters = "",
-            name = "",
-            order = 0,
-            weight = 0
-        )
-
-        private val TEST_POKEMON_DETAIL_SCHEMA: PokemonDetailSchema = PokemonDetailSchema(
-            abilities = listOf(),
-            baseExperience = 0,
-            forms = listOf(),
-            gameIndices = listOf(),
-            height = 0,
-            heldItems = listOf(),
-            id = 0,
-            isDefault = false,
-            locationAreaEncounters = "",
-            moves = listOf(),
-            name = "",
-            order = 0,
-            pastTypes = listOf(),
-            species = Species(name = "", url = ""),
-            sprites = Sprites(
-                backDefault = "",
-                backFemale = null,
-                backShiny = "",
-                backShinyFemale = null,
-                frontDefault = "",
-                frontFemale = null,
-                frontShiny = "",
-                frontShinyFemale = null,
-                other = Other(
-                    dreamWorld = DreamWorld(
-                        frontDefault = "",
-                        frontFemale = null
-                    ), home = Home(
-                        frontDefault = "",
-                        frontFemale = null,
-                        frontShiny = "",
-                        frontShinyFemale = null
-                    ), officialArtwork = OfficialArtwork(frontDefault = "")
-                ),
-                versions = Versions(
-                    generationI = GenerationI(
-                        redBlue = RedBlue(
-                            backDefault = "",
-                            backGray = "",
-                            frontDefault = "",
-                            frontGray = ""
-                        ), yellow = Yellow(
-                            backDefault = "",
-                            backGray = "",
-                            frontDefault = "",
-                            frontGray = ""
-                        )
-                    ), generationIi = GenerationIi(
-                        crystal = Crystal(
-                            backDefault = "",
-                            backShiny = "",
-                            frontDefault = "",
-                            frontShiny = ""
-                        ), gold = Gold(
-                            backDefault = "",
-                            backShiny = "",
-                            frontDefault = "",
-                            frontShiny = ""
-                        ), silver = Silver(
-                            backDefault = "",
-                            backShiny = "",
-                            frontDefault = "",
-                            frontShiny = ""
-                        )
-                    ), generationIii = GenerationIii(
-                        emerald = Emerald(frontDefault = "", frontShiny = ""),
-                        fireredLeafgreen = FireredLeafgreen(
-                            backDefault = "",
-                            backShiny = "",
-                            frontDefault = "",
-                            frontShiny = ""
-                        ),
-                        rubySapphire = RubySapphire(
-                            backDefault = "",
-                            backShiny = "",
-                            frontDefault = "",
-                            frontShiny = ""
-                        )
-                    ), generationIv = GenerationIv(
-                        diamondPearl = DiamondPearl(
-                            backDefault = "",
-                            backFemale = null,
-                            backShiny = "",
-                            backShinyFemale = null,
-                            frontDefault = "",
-                            frontFemale = null,
-                            frontShiny = "",
-                            frontShinyFemale = null
-                        ), heartgoldSoulsilver = HeartgoldSoulsilver(
-                            backDefault = "",
-                            backFemale = null,
-                            backShiny = "",
-                            backShinyFemale = null,
-                            frontDefault = "",
-                            frontFemale = null,
-                            frontShiny = "",
-                            frontShinyFemale = null
-                        ), platinum = Platinum(
-                            backDefault = "",
-                            backFemale = null,
-                            backShiny = "",
-                            backShinyFemale = null,
-                            frontDefault = "",
-                            frontFemale = null,
-                            frontShiny = "",
-                            frontShinyFemale = null
-                        )
-                    ), generationV = GenerationV(
-                        blackWhite = BlackWhite(
-                            animated = Animated(
-                                backDefault = "",
-                                backFemale = null,
-                                backShiny = "",
-                                backShinyFemale = null,
-                                frontDefault = "",
-                                frontFemale = null,
-                                frontShiny = "",
-                                frontShinyFemale = null
-                            ),
-                            backDefault = "",
-                            backFemale = null,
-                            backShiny = "",
-                            backShinyFemale = null,
-                            frontDefault = "",
-                            frontFemale = null,
-                            frontShiny = "",
-                            frontShinyFemale = null
-                        )
-                    ), generationVi = GenerationVi(
-                        omegarubyAlphasapphire = OmegarubyAlphasapphire(
-                            frontDefault = "",
-                            frontFemale = null,
-                            frontShiny = "",
-                            frontShinyFemale = null
-                        ), xY = XY(
-                            frontDefault = "",
-                            frontFemale = null,
-                            frontShiny = "",
-                            frontShinyFemale = null
-                        )
-                    ), generationVii = GenerationVii(
-                        icons = Icons(frontDefault = "", frontFemale = null),
-                        ultraSunUltraMoon = UltraSunUltraMoon(
-                            frontDefault = "",
-                            frontFemale = null,
-                            frontShiny = "",
-                            frontShinyFemale = null
-                        )
-                    ), generationViii = GenerationViii(
-                        icons = IconsX(
-                            frontDefault = "",
-                            frontFemale = null
-                        )
-                    )
-                )
-            ),
-            stats = listOf(),
-            types = listOf(),
-            weight = 0
-        )
     }
 }
